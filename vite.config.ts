278c32fa52538ea4302f983b687c2234c3990ebd
umeshsatyapal <50564654+umeshsatyapal@users.noreply.github.com>
2026-01-23 02:09:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
-    },
-    build: {
-      outDir: 'dist',
-    }
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+  // ⚠️ This line is the fix for your flat folder structure
+  root: './',
+  build: {
+    outDir: 'dist',
+  }
+})
