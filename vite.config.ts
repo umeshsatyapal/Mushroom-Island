09d242a5f6c72516f46d945e278fd6989e044393
umeshsatyapal <50564654+umeshsatyapal@users.noreply.github.com>
2026-01-23 02:26:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ This line is the fix for your flat folder structure
   root: './',
   build: {
     outDir: 'dist',
